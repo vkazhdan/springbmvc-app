1da9fdc8c0d4aa81e461332bed24c2094d4720b1
vkazhdan <vkazhdan@anotheria.net>
2019-01-02 14:54:55 +0200

    Spring Security: Added Roles to Registration Page

--- src/main/java/com/springb/controller/RegistrationController.java
+++ src/main/java/com/springb/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import com.springb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,15 +26,21 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
+        // Validate
+        if (CollectionUtils.isEmpty(user.getRoles())) {
+            model.addAttribute("message", "Role(s) not selected!");
+            return "registration";
+        }
+
         if (userService.isUserExistByUsername(user.getUsername())) {
             model.addAttribute("message", String.format("User: '%s' already exists!", user.getUsername()));
             return "registration";
         }
 
+        // Create
         user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
         userService.createUser(user);
 
-        return "redirect:/registration";
+        return "redirect:/login";
     }
 }
